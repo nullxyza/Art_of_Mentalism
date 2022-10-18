b290b3307e79ee351c949b8dbfd246c95e147c42
nullxyza <donmilton123@gmail.com>
2022-10-18 17:11:52 +0530

    divider progressbar scrollreveal

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// scroll top btn reveal
+let mybutton = document.getElementById("scrltop-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction(), myFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+//  scroll top btn reveal ends here
+
+// scroll progress bar
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
